e11e2a0ebffa44e2f29b5801f2a35b8cfe58b761
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-03-13 23:45:07 +0330

    add page menu module

---
+++ src/components/PageMenu/PageMenu.jsx
@@ -0,0 +1,81 @@
+import {
+  HowToReg,
+  Login,
+  Logout,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
+import { Badge } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+import { logout } from "../../redux/userSlice";
+import { userRequest } from "../../requestMethods";
+
+const PageMenu = ({
+  ItemClassName,
+  TitlesClassName,
+  SelectedClassName,
+  currentUser,
+}) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    userRequest.defaults.headers.token = "";
+  };
+
+  return (
+    <>
+      <div
+        className={`${ItemClassName} ${
+          location.pathname === "/cart" ? SelectedClassName : ""
+        }`}
+        onClick={() => navigate("/cart")}
+      >
+        <span className={TitlesClassName}>Cart</span>
+        <Badge badgeContent={2} color="primary" overlap="rectangular">
+          <ShoppingCartOutlined />
+        </Badge>
+      </div>
+
+      {currentUser ? (
+        <>
+          <div
+            className={`${ItemClassName} ${
+              location.pathname === "/login" ? SelectedClassName : ""
+            }`}
+            onClick={handleLogout}
+          >
+            <span className={TitlesClassName}>Logout</span>
+            <Logout />
+          </div>
+        </>
+      ) : (
+        <>
+          <div
+            className={`${ItemClassName} ${
+              location.pathname === "/register" ? SelectedClassName : ""
+            }`}
+            onClick={() => navigate("/register")}
+          >
+            <span className={TitlesClassName}>Register</span>
+            <HowToReg />
+          </div>
+
+          <div
+            className={`${ItemClassName} ${
+              location.pathname === "/login" ? SelectedClassName : ""
+            }`}
+            onClick={() => navigate("/login")}
+          >
+            <span className={TitlesClassName}>Sign In</span>
+            <Login />
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default PageMenu;
